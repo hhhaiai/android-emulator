c13a8d7ca9789013626f69eb932a08bf269d9e58
sanbo <sanbo.xyz@gmail.com>
2022-02-08 18:49:51 +0800

    update sth. at 2022-02-08 18:49:51

---
+++ src/main/java/me/hhhaiai/aemulator/runner/AndroidEmulatorHelper.java
@@ -0,0 +1,17 @@
+package me.hhhaiai.aemulator.runner;
+
+/**
+ * @Copyright © 2022 sanbo Inc. All rights reserved.
+ * @Description: TODO
+ * @Version: 1.0
+ * @Create: 2022/2/8 6:39 PM
+ * @author: sanbo
+ */
+public class AndroidEmulatorHelper {
+    public static boolean createEmulator() {
+        //1. ANDROID_SDK_ROOT android sdk安装路径
+        // https://developer.android.com/studio/command-line/variables?hl=zh-cn
+
+        return false;
+    }
+}
--- src/main/java/me/hhhaiai/aemulator/runner/MainApplication.java
+++ src/main/java/me/hhhaiai/aemulator/runner/MainApplication.java
@@ -1,5 +1,7 @@
 package me.hhhaiai.aemulator.runner;
 
+import java.io.IOException;
+
 /**
  * @Copyright © 2022 sanbo Inc. All rights reserved.
  * @Description: TODO
@@ -8,4 +10,40 @@ package me.hhhaiai.aemulator.runner;
  * @author: sanbo
  */
 public class MainApplication {
+    public static void main(String[] args) {
+
+
+    }
+
+    private static void help() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("command download and install android emulator:").append("\r\n")
+                .append("\t-api-level:")
+        ;
+
+        System.out.println(sb.toString());
+    }
+
+
+    public static void prepare(String[] commands) {
+        // create emulator
+        if (AndroidEmulatorHelper.createEmulator()) {
+            // run commands
+            prepare(commands);
+        }
+    }
+
+
+
+    private static void runcommand(String[] commands) {
+        if (commands != null && commands.length > 0) {
+            for (String cmd : commands) {
+                try {
+                    Runtime.getRuntime().exec(cmd);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
